739c2ac9277f5cb6d55a083d51b95e0a59f8d792
Vishruti Ganesh <vishruti@mit.edu>
2021-09-18 11:53:14 -0400

    idk

--- app/controllers/email/views.py
+++ app/controllers/email/views.py
@@ -2,6 +2,7 @@ from flask import Blueprint, Response, request, jsonify, make_response, render_t
 from config import config_settings
 import requests
 import config
+import os
 
 email_bp = Blueprint("email", __name__, url_prefix='/email')
 
@@ -11,13 +12,18 @@ def test():
     from app import app
     def send_email(mentor_email, team_email, team_id, mentor_id):
         app.config.from_object(config_settings['production'])
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        data_file = os.path.join(basedir, 'email.html')
+        HtmlFile = open(data_file, 'r', encoding='utf-8')
+        source_code = HtmlFile.read()   
+        HtmlFile.close()
         return requests.post(
             "https://api.mailgun.net/v3/my.hackmit.org/messages",
             auth=("api", app.config['MAILGUN_API']),
-            data={"from": "Test User <mentor@my.hackmit.org>",
+            data={"from": "HackMIT <mentor@my.hackmit.org>",
                 "to": [mentor_email, team_email],
-                "subject": "test matches",
-                "text": "goomonin ur team is {0} and mentor is {1}".format(team_id, mentor_id)})
+                "subject": "HackMIT <> Beginner-Mentor Pairing!",
+                "html": source_code})
 
     all_matches = Matches.serialize()
     for row in all_matches:
